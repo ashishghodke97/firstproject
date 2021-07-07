f43e968ca1717dce4a62ae1e36cf6eca63d70e06
ashishghodke97 <srj246ag@gmail.com>
2021-07-07 21:26:46 +0530

    fromTerminal

---
+++ add.c
@@ -0,0 +1,5 @@
+int add(int a,int b)
+{
+    int r=a+b;
+    return r;
+}
---
+++ main.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int add(int a,int b);
+int main()
+{
+printf("enter the 2 numbers %d\n\n",add(6,7));
+return 0;
+}
